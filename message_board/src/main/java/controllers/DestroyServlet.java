ac3d430851457885020e5bbff97398852a62f5c0
dirFY-24hosodarikuto <rikuto.hosoda.b926@dirbato.co.jp>
2024-04-23 15:07:36 +0900

    DestroyServlet.javaを作成

--- message_board/src/main/java/controllers/DestroyServlet.java
+++ message_board/src/main/java/controllers/DestroyServlet.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,16 +11,16 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public DestroyServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -39,22 +37,10 @@ public class UpdateServlet extends HttpServlet {
 			
 			// セッションスコープからメッセージのIDを取得して
 			// 該当のIDのメッセージ1件のみをデータベースから取得
-			var m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+			var m = em.find(Message.class, (Integer) (request.getSession().getAttribute("message_id")));
 			
-			
-			// フォームの内容を各フィールドに上書き
-			var title = request.getParameter("title");
-			m.setTitle(title);
-			
-			var content = request.getParameter("content");
-			m.setContent(content);
-			
-			var currentTime = new Timestamp(System.currentTimeMillis());
-			m.setUpdated_at(currentTime); // 更新日時のみ上書き
-			
-			
-			// データベースを更新
 			em.getTransaction().begin();
+			em.remove(m); //　データ削除
 			em.getTransaction().commit();
 			em.close();
 			
@@ -65,8 +51,10 @@ public class UpdateServlet extends HttpServlet {
 			
 			// indexページへリダイレクト
 			response.sendRedirect(request.getContextPath()+ "/index");
+		
+		
+		
 		}
-
 	}
-}
 
+}
